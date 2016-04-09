2a7e112e08d1a1d34aac71d577a1b2d996ccfb89
Marla Brizel <marlabrizel@gmail.com>
2016-04-09 11:19:11 -0600

    added ring dep and very basic wiring

--- src/todo_clj/core.clj
+++ src/todo_clj/core.clj
@@ -5,3 +5,8 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
