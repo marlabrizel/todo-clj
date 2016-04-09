8aca01f2e65a2e13cff2fad99a6735c42cb12613
Marla Brizel <marlabrizel@gmail.com>
2016-04-09 13:45:47 -0600

    hook up compojure and preliminary routing

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
 (defproject todo-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.4.0"]]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler todo-clj.core/handler}
-  :main ^:skip-aot todo-clj.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :ring {:handler todo-clj.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
