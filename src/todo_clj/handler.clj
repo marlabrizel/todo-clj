bdbd71ad547e8cc505c78bc9dd1854eb2a9c75ce
Marla Brizel <marlabrizel@gmail.com>
2016-04-09 14:27:15 -0600

    Complete Step 2

--- src/todo_clj/handler.clj
+++ src/todo_clj/handler.clj
@@ -1,10 +1,13 @@
 (ns todo-clj.handler
 (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+          [compojure.route :as route]
+          [ring.util.response :as resp]
+          [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (resp/content-type (resp/resource-response  "index.html" {:root "public"}) "text/html"))
+  (route/resources "/")
+  (route/files "public")
   (route/not-found "Not Found"))
 
 (def app
