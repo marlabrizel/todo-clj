ed65f2a9fd53429be2a96aa59526d98b6faa9acd
Marla Brizel <marlabrizel@gmail.com>
2016-04-09 18:15:06 -0600

    hook up postgres

--- project.clj
+++ project.clj
@@ -6,9 +6,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [org.clojure/java.jdbc "0.4.1"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler todo-clj.handler/app}
+  :main ^:skip-aot todo-clj.handler
+  :ring {:handler todo-clj.handler/app
+         :init todo_clj.models.migration/migrate}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]]}
+   :uberjar {:aot :all}})
