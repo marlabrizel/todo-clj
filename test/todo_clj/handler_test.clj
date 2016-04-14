cef7d7bc3d7c85ca9418c9fdbb1dc4d5f7cfbac9
Marla Brizel <marlabrizel@gmail.com>
2016-04-13 18:34:58 -0600

    add some real tests

--- test/todo_clj/handler_test.clj
+++ test/todo_clj/handler_test.clj
@@ -1,7 +1,17 @@
 (ns todo-clj.handler-test
   (:require [clojure.test :refer :all]
-            [todo-clj.handler :refer :all]))
+            [ring.mock.request :as mock]
+            [todo-clj.handler :as handler]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest test-app
+  (testing "home endpoint"
+    (let [response (handler/app (mock/request :get "/"))]
+    (is (= (get-in response [:status]) 200))))
+
+  (testing "about endpoint"
+    (let [response (handler/app (mock/request :get "/about"))]
+    (is (= (get-in response [:status]) 200))))
+
+  (testing "a non-existant route"
+    (let [response (handler/app (mock/request :get "/not-here"))]
+    (is (= (get-in response [:status]) 404)))))
